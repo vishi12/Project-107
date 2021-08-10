e64f0fadd093625186e865c5770bb32f2a9d21f2
vishi12 <77247243+vishi12@users.noreply.github.com>
2021-08-10 12:46:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5_version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/s8_lhStUd/model.json', modelLoaded);
